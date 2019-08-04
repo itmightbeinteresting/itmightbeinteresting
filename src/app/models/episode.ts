c523db26bae6244951dc67fda05dfcf008789b11
Josh Levy <joshaaronlevy@gmail.com>
2019-08-04 16:41:22 -0600

    Updated new episode and update episode forms

--- src/app/models/episode.ts
+++ src/app/models/episode.ts
@@ -1,11 +1,11 @@
 export interface Episode {
-  added: string;
-  released: boolean;
-  title: string;
-  slug: string;
-  website_url: string;
-  embed_url: string;
-  itunes_url: string;
-  youtube_url: string;
-  spotify_url: string;
+  added?: string;
+  available?: boolean;
+  title?: string;
+  slug?: string;
+  website_url?: string;
+  embed_url?: string;
+  itunes_url?: string;
+  youtube_url?: string;
+  spotify_url?: string;
 }
